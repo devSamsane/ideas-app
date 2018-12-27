279f42753f53d1760f827ad7cedf806617d06b7b
olivier <lavauxo@gmail.com>
2018-12-27 23:48:39 +0100

    Added Error Actions & Reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { AuthService } from '@app/services/auth.service';
 import { ApiService } from '@app/services/api.service';
+import { AppStoreModule } from '@app/store/app-store.module';
 
 @NgModule({
   declarations: [
@@ -18,8 +16,7 @@ import { ApiService } from '@app/services/api.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument()
+    AppStoreModule
   ],
   providers: [
     AuthService,
