4cd0a9a7f2473193a5f331099ac75159c13a0670
olivier <lavauxo@gmail.com>
2018-12-28 18:27:58 +0100

    Added Auth Actions and Reducers

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -5,20 +5,28 @@ import { StoreModule, ActionReducerMap } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { errorReducer, ErrorState } from '@app/store/reducers/errors.reducer';
+import { AuthEffects } from '@app/store/effects/auth.effects';
+import { AuthState, authReducer } from '@app/store/reducers/auth.reducer';
 
 export interface AppState {
   error: ErrorState;
+  auth: AuthState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  error: errorReducer
+  error: errorReducer,
+  auth: authReducer
 };
+
+export const effects = [
+  AuthEffects
+];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot(effects),
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument()
   ]
---
+++ src/app/store/reducers/auth.reducer.ts
@@ -0,0 +1,20 @@
+import { User } from '@app/models/user.model';
+import { Action, AuthActionTypes } from '@app/store/actions/auth.action';
+
+export interface AuthState {
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  user: null
+};
+
+export const authReducer: (state: AuthState, action: Action) => AuthState = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case AuthActionTypes.SET_CURRENT_USER:
+      return { ...state, user: action.payload };
+
+    default:
+      return state;
+  }
+};
